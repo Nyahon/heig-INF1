183074aa7608da1e9e12ded01f88020318bb0ecc
yohann.meyer <yohann.meyer@gmail.com>
2016-11-21 15:54:32 +0100

    First idea

--- main.cpp
+++ main.cpp
@@ -24,11 +24,17 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <limits>
+#include <climits>
 #include <cmath>
+#include <ctime>
+
+#define VIDER_BUFFER cin.ignore(INT_MAX, '\n')
 
 using namespace std;
 
+
+const int WIDTH_TEXT = 20;
+const int WIDTH_INT  = 4;
 /*
  Asks the user if he wants to start the program again
  Checks the validity of the input
@@ -37,53 +43,103 @@ using namespace std;
 
 
 bool doAgain();
-void askUser(string message, int limitMin, int limitMax);
+int input(string message, int limitMin, int limitMax, string error="Mauvaise saisie. Veuillez reessayez");
+void printRoad(int lengthCirc,int amplitude,int widthRoad,int width);
 
 int main() {
-    char YES = 'Y';
-    char answer;
-    string message;
-    
-    
+    srand(time(NULL));
+
+    const int   LENGTH_MIN      = 0,
+                LENGTH_MAX      = 1000,
+                AMP_MIN         = 0,
+                AMP_MAX         = 3,
+                WIDTH_ROAD_MIN  = 3,
+                WIDHT_ROAD_MAX  = 10,
+                WIDTH_MIN       = 20,
+                WIDHT_MAX       = 50;
+
+    char        answer;
+    int         lengthCirc,
+                amplitude,
+                widthRoad,
+                width;
+
     do{
-        
-        
-        
-        
-        
-        
+        cout << "Ce programme affiche une route en fonction de différents paramètres" << endl << endl;
+
+        lengthCirc  = input("Longueur du circuit", LENGTH_MIN, LENGTH_MAX);
+        amplitude   = input("Amplitude max des virages", AMP_MIN, AMP_MAX);
+        widthRoad   = input("Largeur de la route", WIDTH_ROAD_MIN, WIDHT_ROAD_MAX);
+        width       = input("Largeur totale", WIDTH_MIN, WIDHT_MAX);
+
+
+        printRoad(lengthCirc,amplitude,widthRoad,width);
+
+
+
+
+
         answer = doAgain();
     }while(answer);
-    
+
     return EXIT_SUCCESS;
 }
 
 
 bool doAgain(){
-    
+
     char const YES = 'Y';
     char const NO = 'N';
     char answer;
-    
+
     do{
         cout << "Do you want to start again? [y/n]";
         cin >> answer;
-        if(answer == YES){
-            answer = tolower(YES);
-        }
-        }while(answer != YES || answer != tolower(NO) && answer != NO);
-       
-        
-        return(answer==YES);
+
+        }while(toupper(answer) != YES || toupper(answer) != NO);
+
+
+        return(toupper(answer)==YES);
 }
 
-void askUser(string message, int limitMin, int limitMax){
+int input(string message, int limitMin, int limitMax, string error){
     int userInput;
+    bool isValid;
     do{
-        cout << message;
-        cin >> userInput;
-        
+        cout << setw(WIDTH_TEXT) << message << ": [ " << setw(WIDTH_INT) << limitMin << " et " << setw(WIDTH_INT) << limitMax <<"] :" ;
+        isValid = bool(cin >> userInput);
+
+        if(!isValid || userInput < limitMin || userInput > limitMax ){
+            cout << error << endl;
+            cin.clear();
+        }
+        VIDER_BUFFER;
+
     }while(userInput < limitMin || userInput > limitMax);
-    
-    return userInput; 
+
+    return userInput;
 }
+string widthRoad(int width, char CHAR_ROAD){
+    return "e";
+}
+void printRoad(int lengthCirc,int amplitude,int widthRoad,int width){
+
+    const char CHAR_LIM     = '|';
+    const char CHAR_ROAD    = '=';
+    const char CHAR_SPACE   = ' ';
+
+    int sizeSpace = (width-2-widthRoad)/2;
+    int shift;
+    for (int dist=0;dist<lengthCirc;++dist){
+
+        if(dist){
+            shift = rand()%6 -3;
+        }
+        cout << setw(WIDTH_INT) << (dist%10 ? " " : to_string(dist));
+        cout << CHAR_LIM << string(sizeSpace-shift,CHAR_SPACE) << string(widthRoad,CHAR_ROAD) << string(widthRoad-2-sizeSpace+shift,CHAR_SPACE) << CHAR_LIM << endl;
+
+        sizeSpace-=shift;
+    }
+
+}
+
